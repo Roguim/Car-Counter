f3ed85b7d5856ee2c1508985a5871c9ef236cf4c
rhinolary <39052241+Roguim@users.noreply.github.com>
2023-04-29 18:47:03 +1000

    Added total count

--- js/carCounter.js
+++ js/carCounter.js
@@ -3,6 +3,8 @@ let cardButtons = document.getElementsByClassName('cbCar');
 let XMLFile;
 let XMLContents = "";
 
+const refreshFrequency = 5000;
+
 
 document.getElementsByClassName('inputs')[0].addEventListener('dragenter', (event) => {
     event.originalTarget.classList.add('hovered');
@@ -29,9 +31,24 @@ document.getElementsByClassName('editIndex')[0].style.display = "none";
 document.getElementsByClassName('preview')[0].style.backgroundColor = "red";
 document.getElementsByClassName('save')[0].addEventListener("click", save);
 
+function updateCounts() {
+    let cards = document.getElementsByClassName('card');
+
+    let count = 0;
+    for (i = 0; i < cards.length; i++) {
+        count += Number.parseInt(cards[i].children[3].children[1].value);
+    }
+    document.getElementById('totalCount').innerHTML = "";
+    document.getElementById('totalCount').append(document.createTextNode(count));
+}
+
 function newCounter() {
     document.getElementsByClassName('fileScreen')[0].style.display = "none" ;
     document.getElementsByClassName('cardContainer')[0].style.display = "flex" ;
+    document.getElementsByTagName('header')[0].style.display = "block" ;
+
+    updateCounts()
+    window.setInterval(updateCounts, refreshFrequency);
 }
 
 function dragging(event) {
@@ -146,6 +163,10 @@ function createCards(file) {
 
         document.getElementsByClassName('fileScreen')[0].style.display = "none";
         document.getElementsByClassName('cardContainer')[0].style.display = "flex";
+        document.getElementsByTagName('header')[0].style.display = "block" ;
+
+        updateCounts()
+        window.setInterval(updateCounts, refreshFrequency);
 
         document.getElementsByClassName('inputs')[0].classList.remove('hovered');
     } catch (err) {
